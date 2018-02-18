96cfba540a6ed42a05154fcdcfc09a1ee78e1dbf
DESKTOP-643E926\iris <iris.balcazar@est.fib.upc.edu>
2018-02-18 08:32:21 +0100

    info

--- simulation/car.js
+++ simulation/car.js
@@ -1,8 +1,8 @@
-// Initial values: pos:{x:200, y:100}, v:{0,1}, a:{0,0}
+// Initial values: pos:{x:200, y:100}, v:{0,1}, a:{0,0}, playing, in_roundabout
 var car = []
 
 function init() {
-	car = [{x:200, y:100}, {x:0, y:1}, {x:0, y:0}, true]
+	car = [{x:200, y:100}, {x:0, y:1}, {x:0, y:0}, true, false]
 }
 
 
